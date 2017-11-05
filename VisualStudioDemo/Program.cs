8a147468a9220b3f8ebc5ce538aee7e56c10acc1
Emily Reller <relleremily@hotmail.com>
2017-11-05 15:09:03 -0600

    Add message

--- VisualStudioDemo/Program.cs
+++ VisualStudioDemo/Program.cs
@@ -2,11 +2,12 @@
 
 namespace VisualStudioDemo
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Visual Studio Demo");
+            Console.ReadLine();
         }
     }
 }
